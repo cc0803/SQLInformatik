765f6afa9543fef068bf07e00f1e639409bda45b
cc0803 <110593023+cc0803@users.noreply.github.com>
2023-11-05 15:03:36 +0000

    sqlite query

---
+++ sql/mysql.sql
@@ -0,0 +1,5 @@
+-- CREATE TABLE countries(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, capitol TEXT); 
+
+-- INSERT INTO countries(id, name, capitol) VALUES (1, "Germany", "Berlin");
+
+SELECT * FROM countries;
